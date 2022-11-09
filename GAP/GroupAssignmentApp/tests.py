5cc029d6ca7c95c25d1ddf7571c1b580e3a1b9c1
JohnMcKane <jmckane07@qub.ac.uk>
2022-11-09 18:25:35 +0000

    Tests and Class added

--- GAP/GroupAssignmentApp/tests.py
+++ GAP/GroupAssignmentApp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Person
 
 # Create your tests here.
+class PersonTests(TestCase):
+        def test_string_cast_returns_name(self):
+                person = Person(name="Test Person")
+                self.assertEqual("Test Person",str(person))
+        def test_create_person(self):
+                person_count = len(Person.objects.all())
+                for i in range(0,10):
+                    Person.objects.create(name="test",email="test@domain.com")
+                    self.assertEqual(person_count+i+1,len(Person.objects.all()))
