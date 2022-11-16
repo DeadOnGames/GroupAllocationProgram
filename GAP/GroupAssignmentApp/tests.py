a4c62512568d21ff22bd3a47567a3b9174d252ce
JohnMckane <54290793+JohnMckane@users.noreply.github.com>
2022-11-16 17:49:09 +0000

    Update tests.py
    Added comments explaining purpose of each test.

--- GAP/GroupAssignmentApp/tests.py
+++ GAP/GroupAssignmentApp/tests.py
@@ -3,21 +3,24 @@ from .models import Person
 
 # Create your tests here.
 class PersonTests(TestCase):
-        #tests string cast method
+        #Test that string cast method returns name of Person.
         def test_string_cast_returns_name(self):
                 person = Person(name="Test Person")
                 self.assertEqual("Test Person",str(person))
-        #tests creation of person object
+        #Test that creation of person object saves new person to the Data Base.
         def test_create_person(self):
                 person_count = len(Person.objects.all())
                 for i in range(0,10):
                     #Person.objects.create is auto generated constructor, saves instance to data base
                     Person.objects.create(name="test",email="test{0}@domain.com".format(i))
                     self.assertEqual(person_count+i+1,len(Person.objects.all()))
+        #Test that it is possible to create a Person object, and save it to data base, even if person has no email address
+        #Potential use-case is if someone is testing web-app but has not created account.
         def test_no_email_person(self):
                 Person.objects.create(name="john smith")
                 Person.objects.create(name="john stewart")
                 self.assertEqual(len(Person.objects.all()),2)
+        #Test that if there is an attempt to save two Person objects, both with the same email to the data-base, that this fails and throws an exception.
         def test_attempt_save_two_people_same_email(self):
                 p1 = Person(name = "Joe Blogs", email = "jblogs@domain.com")
                 p2 = Person(name = "Jane Blogs", email = "jblogs@domain.com")
