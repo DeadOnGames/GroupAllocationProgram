c7185035e511abfe1456fa8bd3985823a0bf9451
HLavery10 <hlavery10@qub.ac.uk>
2022-11-16 13:43:25 +0000

    Neighbour function

--- GAP/GroupAssignmentApp/GenerateNeighbour.py
+++ GAP/GroupAssignmentApp/GenerateNeighbour.py
@@ -1,19 +1,28 @@
-import numpy as np
-
 
 def generate_neighbour(td):
-
+    perm = []
     l = len(td)
+    print("array", td)
     if l == 0:
         return [[]]
 
     for i in range(l):
-        tdl = td[i]
-        m = len(tdl)
         tdi = td[i]
-        for j in range(m):
-            if j == 0:
-
         
+        m = len(tdi)
+        if m == 1:
+            perm.append(tdi)
+        for j in range(m):
+            o = tdi[j]
+            for k in range(j,m-1):
+                n = tdi[k+1]
+                print("n", n)
+                tdinew = tdi
+                tdinew[k+1] = o
+                tdinew[j] = n
+                print(tdinew)
+                perm.append(tdinew)
 
-generate_neighbour([[1,2,3]])
+    return perm
+
+print(generate_neighbour([[1,2,3]]))
