ba069e5ba93b78774d2df471ad7c8782bfcdce8c
40257102 <ldoherty42@qub.ac.uk>
2022-11-17 11:26:05 +0000

    added test for unapprove function within group class

--- GAP/GroupAssignmentApp/tests.py
+++ GAP/GroupAssignmentApp/tests.py
@@ -16,8 +16,15 @@ class PersonTests(TestCase):
                     self.assertEqual(person_count+i+1,len(Person.objects.all()))
                     
 class GroupTests(TestCase):
+    #tests if approve function correctly changes isApproved variable
     def test_approve(self):
         g = Group.objects.create()
         self.assertFalse(g.isApproved)
         g.approve()
+        self.assertTrue(g.isApproved)
+    #tests if unapprove function correctly changes isApproved variable
+    def test_unapprove(self):
+        g = Group.objects.create()
+        self.assertFalse(g.isApproved)
+        g.unapprove()
         self.assertTrue(g.isApproved)
