ac9361fa01334cb9bb2ea8bd1fe2e101bea590ca
JohnMcKane <jmckane07@qub.ac.uk>
2022-11-20 22:47:58 +0000

    Refactor commit, ran black deleted commented out code, changed name of input variable to iter_2d

--- GAP/GroupAssignmentApp/GenerateNeighbour.py
+++ GAP/GroupAssignmentApp/GenerateNeighbour.py
@@ -1,40 +1,19 @@
-
-
-def generate_neighbours(twoD):
+def generate_neighbours(iter_2d):
     perm = []
     # perm = 3d array to be returned
-    # twoD = 2d list
-    if len(twoD) == 1:
+    # iter_2d = 2d iterable
+    if len(iter_2d) == 1:
         # if only one group is submitted, group is returned with no change
-        return [twoD]
-    elif len(twoD) == 0:
+        return [iter_2d]
+    elif len(iter_2d) == 0:
         # if no groups entered, warning message given
         return "Error: No groups submitted"
 
-    
-    for i in range(len(twoD)-1):
+    for i in range(len(iter_2d) - 1):
 
-        twoDC = twoD.copy()
+        twoDC = iter_2d.copy()
         iList = twoDC[i]
-        iList2 = twoDC[i+1]
+        iList2 = twoDC[i + 1]
         for j in range(len(iList)):
             for k in range(len(iList2)):
                 return perm
-
-
-
-        #print(i)
-        #twoDC = twoD.copy()
-        #iList = twoDC[i]
-        # iList is the i-th list in twoD
-        #elem = iList[i+1]
-        # elem is the i-th element of the i-th list
-        #for j in range(i,len(twoD)-1):
-            # jList is the j\+!-th list of twoD
-         #   jList = twoDC[j+1]
-          #  for k in range(len(twoDC[j])):
-           #     twoDC = twoD.copy()
-            #    elemJ = jList[k]
-             #   twoDC[i][i+1] = elemJ
-              #  twoDC[j+1][k] = elem
-               # perm.append(twoDC)
--- GAP/GroupAssignmentApp/tests.py
+++ GAP/GroupAssignmentApp/tests.py
@@ -4,73 +4,80 @@ from .models import Person, Group, Supervisor
 from .GenerateNeighbour import generate_neighbours
 
 
-
 # Create your tests here.
 class PersonTests(TestCase):
-        #Test that string cast method returns name of Person.
-        def test_string_cast_returns_name(self):
-                person = Person(name="Test Person")
-                self.assertEqual("Test Person",str(person))
-        #Test that creation of person object saves new person to the Data Base.
-        def test_create_person(self):
-                person_count = len(Person.objects.all())
-                for i in range(0,10):
-                    #Person.objects.create is auto generated constructor, saves instance to data base
-                    Person.objects.create(name="test",email="test{0}@domain.com".format(i))
-                    self.assertEqual(person_count+i+1,len(Person.objects.all()))
-        #Test that it is possible to create a Person object, and save it to data base, even if person has no email address
-        #Potential use-case is if someone is testing web-app but has not created account.
-        def test_no_email_person(self):
-                Person.objects.create(name="john smith")
-                Person.objects.create(name="john stewart")
-                self.assertEqual(len(Person.objects.all()),2)
-        #Test that if there is an attempt to save two Person objects, both with the same email to the data-base, that this fails and throws an exception.
-        def test_attempt_save_two_people_same_email(self):
-                p1 = Person(name = "Joe Blogs", email = "jblogs@domain.com")
-                p2 = Person(name = "Jane Blogs", email = "jblogs@domain.com")
-                p1.save()
-                self.assertEqual(len(Person.objects.all()),1)
-                exceptionRaised = False
-                try:
-                        p2.save()
-                except Person.InvalidEmailException  as e:
-                        self.assertEqual(e.message, Person.InvalidEmailException.message)
-                        exceptionRaised = True
-                self.assertEqual(len(Person.objects.all()),1)
-                self.assertTrue(exceptionRaised)
+    # Test that string cast method returns name of Person.
+    def test_string_cast_returns_name(self):
+        person = Person(name="Test Person")
+        self.assertEqual("Test Person", str(person))
 
-#Test Person View
+    # Test that creation of person object saves new person to the Data Base.
+    def test_create_person(self):
+        person_count = len(Person.objects.all())
+        for i in range(0, 10):
+            # Person.objects.create is auto generated constructor, saves instance to data base
+            Person.objects.create(name="test", email="test{0}@domain.com".format(i))
+            self.assertEqual(person_count + i + 1, len(Person.objects.all()))
+
+    # Test that it is possible to create a Person object, and save it to data base, even if person has no email address
+    # Potential use-case is if someone is testing web-app but has not created account.
+    def test_no_email_person(self):
+        Person.objects.create(name="john smith")
+        Person.objects.create(name="john stewart")
+        self.assertEqual(len(Person.objects.all()), 2)
+
+    # Test that if there is an attempt to save two Person objects, both with the same email to the data-base, that this fails and throws an exception.
+    def test_attempt_save_two_people_same_email(self):
+        p1 = Person(name="Joe Blogs", email="jblogs@domain.com")
+        p2 = Person(name="Jane Blogs", email="jblogs@domain.com")
+        p1.save()
+        self.assertEqual(len(Person.objects.all()), 1)
+        exceptionRaised = False
+        try:
+            p2.save()
+        except Person.InvalidEmailException as e:
+            self.assertEqual(e.message, Person.InvalidEmailException.message)
+            exceptionRaised = True
+        self.assertEqual(len(Person.objects.all()), 1)
+        self.assertTrue(exceptionRaised)
+
+
+# Test Person View
 class PersonViewTests(TestCase):
-        #Test 404 error returned if person view requested for does not exist
-        def test_person_404(self):
-                response = self.client.get(reverse("gaa:user", args = (1,)))
-                self.assertEqual(response.status_code, 404)
-        #Tests person view responds correctly if one Person created.
-        def test_person_view(self):
-                Person.objects.create(name = "test name",email = "test@domain.com")
-                response = self.client.get(reverse("gaa:user", args=(1,)))
-                self.assertContains(response, "Name: test name")
-                self.assertContains(response, "Email: test@domain.com")
+    # Test 404 error returned if person view requested for does not exist
+    def test_person_404(self):
+        response = self.client.get(reverse("gaa:user", args=(1,)))
+        self.assertEqual(response.status_code, 404)
+
+    # Tests person view responds correctly if one Person created.
+    def test_person_view(self):
+        Person.objects.create(name="test name", email="test@domain.com")
+        response = self.client.get(reverse("gaa:user", args=(1,)))
+        self.assertContains(response, "Name: test name")
+        self.assertContains(response, "Email: test@domain.com")
+
 
 class GroupTests(TestCase):
-    #tests if approve function correctly changes isApproved variable
+    # tests if approve function correctly changes isApproved variable
     def test_approve(self):
         g = Group.objects.create()
         self.assertFalse(g.isApproved)
         g.approve()
         self.assertTrue(g.isApproved)
-    #tests if unapprove function correctly changes isApproved variable
+
+    # tests if unapprove function correctly changes isApproved variable
     def test_unapprove(self):
         g = Group.objects.create()
         g.approve()
         self.assertTrue(g.isApproved)
         g.unapprove()
         self.assertFalse(g.isApproved)
+
+
 class GenerateNeighboursTests(TestCase):
     def test_one_array_input(self):
-        test_arr = [[1,2,3]]
+        test_arr = [[1, 2, 3]]
         nhbrs = generate_neighbours(test_arr)
-        #check nhbrs is one element array, with test_arr being said element.
-        self.assertEqual(nhbrs[0],test_arr)
-        self.assertEqual(len(nhbrs),1)
-
+        # check nhbrs is one element array, with test_arr being said element.
+        self.assertEqual(nhbrs[0], test_arr)
+        self.assertEqual(len(nhbrs), 1)
