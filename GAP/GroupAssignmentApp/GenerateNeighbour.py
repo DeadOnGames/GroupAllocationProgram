9a4cd8650069b2720b8dc276aacbc363e84a9efa
JohnMcKane <jmckane07@qub.ac.uk>
2022-11-20 23:48:13 +0000

    Modifies gereate neighbour, tests now pass.

--- GAP/GroupAssignmentApp/GenerateNeighbour.py
+++ GAP/GroupAssignmentApp/GenerateNeighbour.py
@@ -1,6 +1,7 @@
+from copy import deepcopy
 def generate_neighbours(iter_2d):
-    perm = []
-    # perm = 3d array to be returned
+    iter_3d = []
+    # iter_3d = 3d array to be returned
     # iter_2d = 2d iterable
     if len(iter_2d) == 1:
         # if only one group is submitted, group is returned with no change
@@ -8,12 +9,15 @@ def generate_neighbours(iter_2d):
     elif len(iter_2d) == 0:
         # if no groups entered, warning message given
         return "Error: No groups submitted"
-
-    for i in range(len(iter_2d) - 1):
-
-        twoDC = iter_2d.copy()
-        iList = twoDC[i]
-        iList2 = twoDC[i + 1]
-        for j in range(len(iList)):
-            for k in range(len(iList2)):
-                return perm
+    for i in range(0,len(iter_2d)-1):
+        for j in range(i+1,len(iter_2d)):
+            for x in range(0,len(iter_2d[i])):
+                for y in range(0,len(iter_2d[j])):
+                    if len(iter_2d[i]) != 2 or len(iter_2d[j]) != 2 or x+y != 2:
+                        neighbour = deepcopy(iter_2d)
+                        neighbour[i][x] = iter_2d[j][y]
+                        neighbour[j][y] = iter_2d[i][x]
+                        iter_3d.append(neighbour)
+                else:
+                    continue
+    return iter_3d
