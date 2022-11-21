008fdbb2d5b952badad4c2544f0fc762f85c14f6
JohnMcKane <jmckane07@qub.ac.uk>
2022-11-21 11:30:34 +0000

    Ran black again.

--- GAP/GroupAssignmentApp/views.py
+++ GAP/GroupAssignmentApp/views.py
@@ -3,6 +3,6 @@ from .models import Person
 
 # Create your views here.
 def person_view(request, person_id):
-        person = get_object_or_404(Person, pk=person_id) 
-        context = {"name":person.name, "email": person.email}
-        return render(request, "GAP/user.html", context)
+    person = get_object_or_404(Person, pk=person_id)
+    context = {"name": person.name, "email": person.email}
+    return render(request, "GAP/user.html", context)
