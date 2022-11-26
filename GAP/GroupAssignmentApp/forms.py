4758a8745a7617968282e45968b310264baf63ac
JohnMcKane <jmckane07@qub.ac.uk>
2022-11-26 19:45:02 +0000

    Added selection of participants

--- GAP/GroupAssignmentApp/forms.py
+++ GAP/GroupAssignmentApp/forms.py
@@ -1,6 +1,21 @@
 from django import forms
+from .models import Participant
 
 
 class PersonForm(forms.Form):
-    first_name = forms.CharField(label="first_name", max_length=20)
-    last_name = forms.CharField(max_length=20, label="last_name")
+    def class_list():
+        participants = []
+        i = 1
+        for p in Participant.objects.all():
+            if p.email != "" and p.name != "":
+                i += 1
+                participants.append((i, p))
+        return participants
+
+    first_name = forms.CharField(label="First Name:", max_length=20)
+    last_name = forms.CharField(max_length=20, label="Last Name:")
+    wants_notified = forms.BooleanField(label="Notify Me when Groups Allocated:")
+    email_address = forms.CharField(max_length=50, label="Email:")
+    preferences = forms.ChoiceField(
+        choices=class_list(), label="Who do you want to work with?"
+    )
